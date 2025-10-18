b5b18c97dbf913f318c353563e7b658be510f29f
Jackson <jacksonbryan280@gmail.com>
2025-10-17 23:13:05 -0400

    Created hover and click stats in js for lock icons

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 const locks = document.querySelectorAll(".lock-icon");
-let isPressed = false;
-let isHovered = false;
+
+// let isHovered = false;
 locks.forEach((lock) => {
+  let isPressed = false;
   lock.addEventListener("click", () => {
     if (isPressed === false) {
       lock.style.backgroundImage = "url(images/lock-icon-selected.svg)";
@@ -11,6 +12,19 @@ locks.forEach((lock) => {
       isPressed = false;
     }
     console.log("lock clicked");
+  });
+  lock.addEventListener("mouseenter", () => {
+    // if (isPressed === false) {
+    //   lock.style.backgroundImage = "url(images/lock-icon-hover.svg)";
+    // }
+    lock.style.backgroundImage = "url(images/lock-icon-hover.svg)";
+  });
+  lock.addEventListener("mouseleave", () => {
+    if (isPressed === false) {
+      lock.style.backgroundImage = "url(images/lock-icon.svg)";
+    } else if (isPressed === true) {
+      lock.style.backgroundImage = "url(images/lock-icon-selected.svg)";
+    }
   });
 });
 
