db5d12a713c5bcd89ba52eab165c2e12deae9176
Jackson <jacksonbryan280@gmail.com>
2025-10-17 22:51:04 -0400

    Have toggling lock in js working

--- javascript.js
+++ javascript.js
@@ -1,2 +1,25 @@
+const locks = document.querySelectorAll(".lock-icon");
+let isPressed = false;
+let isHovered = false;
+locks.forEach((lock) => {
+  lock.addEventListener("click", () => {
+    if (isPressed === false) {
+      lock.style.backgroundImage = "url(images/lock-icon-selected.svg)";
+      isPressed = true;
+    } else if (isPressed === true) {
+      lock.style.backgroundImage = "url(images/lock-icon.svg)";
+      isPressed = false;
+    }
+    console.log("lock clicked");
+  });
+});
 
+// const rollBtn = document.querySelector("button");
 
+// let randomNum = 0;
+
+// rollBtn.addEventListener("click", () => {
+//   randomNum = Math.random();
+//   console.log(randomNum);
+//   return;
+// });
